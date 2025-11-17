d1a6eb3a841c227122a2343c1573e451c2876058
FabiolaCastanedaMondragon <fabiolacastanedamondragon2004@gmail.com>
2025-11-17 12:25:09 -0600

    Corregir la ruta WSGI a api_cancer

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-# Salir inmediatamente si falla un comando
-set -e
+# ... (otras líneas)
 
-# Activar el entorno virtual (opcional pero bueno para establecer rutas)
-. .venv/bin/activate || true
-
-# EJECUTAR EL CÓDIGO CON PYTHON
-# Usamos python -m gunicorn para que Python sepa dónde encontrar el paquete,
-# y 'sh' se encargará de encontrar el ejecutable 'python'.
-exec python -m gunicorn cancerpiel.wsgi:application --bind 0.0.0.0:"$PORT"
+# EJECUTAR EL CÓDIGO CON PYTHON (Línea Corregida)
+exec python -m gunicorn **api_cancer**.wsgi:application --bind 0.0.0.0:"$PORT"
